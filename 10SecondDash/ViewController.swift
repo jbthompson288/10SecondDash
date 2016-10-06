32d12ac82531036acfa4d043d5dbed2db8b62b86
jbthompson288 <jbthompson288@gmail.com>
2016-10-06 17:28:21 -0600

    Initial Commit

---
+++ 10SecondDash/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  10SecondDash
+//
+//  Created by JB Thompson on 10/6/16.
+//  Copyright © 2016 JB Thompson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
