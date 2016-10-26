40a1157238f75e1522c5deb9c70626df84195068
jbthompson288 <jbthompson288@gmail.com>
2016-10-25 18:20:43 -0600

    Creating Model Objects and View Controllers
    created Avatar and Game classes, created view controllers for all screens (Main Menu, Avatar, High Scores, Settings)

---
+++ 10SecondDash/Avatar.swift
@@ -0,0 +1,9 @@
+//
+//  Avatar.swift
+//  10SecondDash
+//
+//  Created by JB Thompson on 10/25/16.
+//  Copyright © 2016 JB Thompson. All rights reserved.
+//
+
+import Foundation
---
+++ 10SecondDash/Game.swift
@@ -0,0 +1,9 @@
+//
+//  Game.swift
+//  10SecondDash
+//
+//  Created by JB Thompson on 10/25/16.
+//  Copyright © 2016 JB Thompson. All rights reserved.
+//
+
+import Foundation
