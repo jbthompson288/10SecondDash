5ce2312bfc04bf4fdbe863ce7e520a2228f71a15
jbthompson288 <jbthompson288@gmail.com>
2016-11-12 08:25:16 -0700

    Controller/Storyboard
    added HighScoresController class and removed Settings views and replaced it with just the Remove Ads Button

---
+++ 10SecondDash/HighScoresController.swift
@@ -0,0 +1,15 @@
+//
+//  HighScoresController.swift
+//  10SecondDash
+//
+//  Created by JB Thompson on 11/12/16.
+//  Copyright © 2016 JB Thompson. All rights reserved.
+//
+
+import Foundation
+
+class HighScoresController {
+    
+    
+    
+}
